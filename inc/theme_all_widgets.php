885ec06c0b5bd041059b754416ddf083f75a5ab5
developer.tajul <developer.tajul@gmail.com>
2018-03-08 12:34:32 +0600

    woocommerce template includes

--- inc/theme_all_widgets.php
+++ inc/theme_all_widgets.php
@@ -1,6 +1,7 @@
 <?php 
 
 function woo_starter_widgets_init() {
+	// blog-sidebar
 	register_sidebar( array(
 		'name'          => esc_html__( 'Sidebar', 'woo-starter' ),
 		'id'            => 'sidebar-1',
@@ -10,5 +11,17 @@ function woo_starter_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+
+	// shop page widget
+	register_sidebar( array(
+		'name'          => esc_html__( 'Shop Sidebar', 'woo-starter' ),
+		'id'            => 'shop-sidebar-1',
+		'description'   => esc_html__( 'Add widgets here.', 'woo-starter' ),
+		'before_widget' => '<div id="%1$s" class="widget %2$s single-sidebar-amar-bazar">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget-title site-bar-title">',
+		'after_title'   => '</h3>',
+	) );
+
 }
 add_action( 'widgets_init', 'woo_starter_widgets_init' );
---
+++ woocommerce.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+
+
+
+
+<div class="main-product-area three">
+    <div class="container">
+		<div class="row product-list--1">
+
+
+
+			<div class="col-sm-3">
+		        <?php dynamic_sidebar('shop-sidebar-1'); ?>
+		    </div>
+
+		
+		  	<?php woocommerce_content(); ?>
+
+
+
+
+
+		</div>
+	</div>
+</div>
+
+
+
+<?php get_footer(); ?>
